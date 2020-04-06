48e5e9313bb8fbec9127e81883f1eb9d98f3e35a
Abhijeet Kumar <49158711+abhijeetgu@users.noreply.github.com>
2020-04-06 14:08:50 +0530

    Create Maximum_SubArray.cpp

---
+++ Maximum_SubArray.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) 
+    {
+        int a=0,m=INT_MIN;
+        for(int x : nums)
+        {
+            a += x;
+            m = max(a,m);
+            a = max(a,0);
+        }
+        return m;
+    }
+};
